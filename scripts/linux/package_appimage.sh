a9c903f9ef182b0b941c139e4ed7887ed718a1aa
vinceTheProgrammer <vincetheprogrammer@gmail.com>
2026-09-21 20:38:37 -0500

    fix macos architecture builds

--- scripts/linux/package_appimage.sh
+++ scripts/linux/package_appimage.sh
@@ -9,8 +9,6 @@ cp target/release/framebolt-desktop AppDir/usr/bin/framebolt
 mkdir -p dist
 cd dist
 
-# need to use modified version with the bundled strip executable removed because the libs on my system that it tries to strip are too new or some shit
-# /tmp/linuxdeploy-modified/linuxdeploy-system-strip.AppImage
 linuxdeploy \
     --appdir ../AppDir \
     --executable ../AppDir/usr/bin/framebolt \
